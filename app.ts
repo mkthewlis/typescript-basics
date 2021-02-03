b7f0cf332c10c5f3bff24a46ccfa9e76f9547a59
mkthewlis <miranda.thewlis@jayway.com>
2021-02-03 14:07:51 +0100

    feat: added example of number, bool and string types

--- app.ts
+++ app.ts
@@ -1,16 +1,21 @@
-function add(n1: number, n2: number) {
+function add(n1: number, n2: number, showResult: boolean, phrase: string ) {
     // Vanilla JS solution where n1 is string can include if statement:
     // if (typeof n1 === 'string' && typeof n2 === 'string') {
     //     return n1 + n2;
     // } else {
     //     throw new Error('Incorrect input!');
     // }
-    return n1 + n2;
+    const result = n1 + n2;
+    if (showResult) {
+        console.log(phrase + result);
+    } else {
+        return result;
+    }
 }
 
-const number1 = '5';
+const number1 = 5; // same as 5.0
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: ';
 
-const result = add(number1, number2);
-
-console.log(result);
+add(number1, number2, printResult, resultPhrase);
