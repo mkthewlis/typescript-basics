42d5885dc9b14e30d6450664ab619c92231e16fa
mkthewlis <miranda.thewlis@jayway.com>
2021-02-03 16:12:32 +0100

    feat: added example of basic object TS notation

--- README.md
+++ README.md
@@ -4,4 +4,12 @@ First project based on the 'Understanding TypeScript - 2021 Edition' course on U
 
 ## Introduction to TypeScript notes:
 
-- TS errors during development, not deployment (unlike JS which shows errors in browser when compiled)
+TS errors during development, not deployment (unlike JS which shows errors in browser when compiled)
+
+
+### Type inference
+- TS is able to infer what type a value is, eg:let x = 3 (here TS infers that x is a number)
+
+- For objects, it infers that it is an object and infers the type of each key value pair
+
+- However, we can also be more specific by declaring the type of everything contained within the object
--- app.ts
+++ app.ts
@@ -1,21 +1,11 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string ) {
-    // Vanilla JS solution where n1 is string can include if statement:
-    // if (typeof n1 === 'string' && typeof n2 === 'string') {
-    //     return n1 + n2;
-    // } else {
-    //     throw new Error('Incorrect input!');
-    // }
-    const result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    } else {
-        return result;
-    }
-}
+// There is the option to be specific, but better to allow TS to infer value types
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+const person = {
+    name: 'Bob',
+    age: 28
+};
 
-const number1 = 5; // same as 5.0
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person);
