870eb8e6ba27d064f51144d9a474a116c72f1056
mkthewlis <miranda.thewlis@jayway.com>
2021-02-03 16:45:16 +0100

    feat: added enums and completed notes on core types

--- README.md
+++ README.md
@@ -6,8 +6,16 @@ First project based on the 'Understanding TypeScript - 2021 Edition' course on U
 
 TS errors during development, not deployment (unlike JS which shows errors in browser when compiled)
 
+*Core types include:*
+- number: 1, 5.6, -10
+- string: 'Hi', "Hi", or with template literals
+- boolean: true, false
+- array: [1, 2, 3]
+- tuple: [1, 'two'] (specifically)
+- enum: enum {NEW, OLD} (create named global identifiers)
+- any: * (should be avoided as much as possible)
 
-### Type inference
+### Notes on core types
 - TS is able to infer what type a value is, eg:let x = 3 (here TS infers that x is a number)
 
 - For objects, it infers that it is an object and infers the type of each key value pair
@@ -15,3 +23,5 @@ TS errors during development, not deployment (unlike JS which shows errors in br
 - However, we can also be more specific by declaring the type of everything contained within the object
 
 - Tuples can be set by overriding TS ability to infer an array, eg. tuple = [number, string]
+
+- Enums allow you to define a set of named constants. This makes it easier to store more about a value by saving the number in the object, plus a name for it, eg enum Example { Three = 3, Four, Five } where Example.Five later also means the number 5.
---
+++ objs-arr-enums.ts
@@ -0,0 +1,59 @@
+// There is the option to be specific, but better to allow TS to infer value types
+// const person: {
+//     name: string;
+//     age: number;
+// } = {
+
+// However, it can be needed when wanting to override TS's own inferrence, eg. when setting 
+// an array to specifically be a tuple. Otherwise, the following would work below the object:
+// person.role.push('admin');
+// person.role[1] = 10;
+// To avoid this, the object can be added again to specify what type role should be:
+
+// const person: {
+//     name: string;
+//     age: number;
+//     hobbies: string[];
+    // This sets role to be a tuple with exactly two elements of these types:
+//     role: [number, string];
+// } = {
+//     name: 'Bob',
+//     age: 28,
+//     hobbies: ['Sports', 'Cooking'],
+//     role: [2, 'Author']
+// };
+
+// Enums assign labels to numbers. Begin by declaring the enum then accessing it globally
+// Declare enums, often written in capital letters
+enum Role { ADMIN, READ_ONLY, AUTHOR };
+
+// Can also include specifics to override 1, 2, 3 etc
+// enum Role { ADMIN = 'Admin', READ_ONLY = 5, AUTHOR = 100};
+
+const person = {
+    name: 'Bob',
+    age: 28,
+    hobbies: ['Sports', 'Cooking'],
+    role: Role.ADMIN
+};
+
+// Use any[] to let the content of the array be anything
+let favouriteActivities: string[];
+favouriteActivities = ['Sports'];
+// This leads to error as it is not an array:
+// favouriteActivities = 'Sports';
+// This leads to an error as it includes a number:
+// favouriteActivies = ['Sports, 1];
+
+// person.nickname would not work as the key doesn't exist
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    // However, this would fail as map only works on number arrays:
+    // console.log(hobby.map(...));
+}
+
+if (person.role === Role.AUTHOR) {
+    console.log('is author')
+};
